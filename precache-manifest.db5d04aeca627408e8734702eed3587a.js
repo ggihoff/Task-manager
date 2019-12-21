a5736a15751e0deaf95ddd28e9efc4ed0f3f1541
ggihoff <52943581+ggihoff@users.noreply.github.com>
2019-12-21 17:41:36 -0300

    initial commit

---
+++ precache-manifest.db5d04aeca627408e8734702eed3587a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a841ac7768352b792dd38fcdcc2f862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a0416d57bec0caa1bb",
+    "url": "/static/css/main.8fe0e662.chunk.css"
+  },
+  {
+    "revision": "aa6173fc2d79ab2eadb1",
+    "url": "/static/js/2.77175816.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.77175816.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63a0416d57bec0caa1bb",
+    "url": "/static/js/main.6705f532.chunk.js"
+  },
+  {
+    "revision": "a7d44f73c41293554bd9",
+    "url": "/static/js/runtime-main.ff13170c.js"
+  }
+]);
